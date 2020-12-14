4933c4d84d90f4e27d99a3d6ac73ed9e6d83e466
Paul Alvarado <palvarado@grupocesa.com>
2020-12-14 14:03:46 -0600

    Se configura y se crea arquitectura basica mvc rutas/vistas para la pagina de ejemplo.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,18 @@ const path = require('path'); //manejador de working directory sin importar OS.
 
 //settings
 app.set('port',4000);
+app.set('views',path.join(__dirname,'views'));
+//app.engine('html',require('ejs').renderFile);
 app.set('view engine','ejs'); //No se necesita instanciarlo, motor de plantillas EJS ya esta integrado en Express js.
 
 //middlewares
 
 //routes
-app.get('/', (req,res) => {
-    //res.send('Hello World'); //Direct/raw output
-    res.sendFile(path.join(__dirname,'views/index.html')); //Html file output
-    //console.log(__dirname + '/views/index.html');
-    //console.log(path.join(__dirname,'views/index.html'));
-});
-
+app.use(require('./routes/index'));
 //static files
+
 
 //listening the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port ',app.get('port'));
+    console.log('Server listening on port ',app.get('port'));
 });
-
-//console.log('Server listening!');
