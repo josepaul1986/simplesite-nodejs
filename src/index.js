39795c4fd44d964b04b369263c479e4e3176c7b3
Paul Alvarado <palvarado@grupocesa.com>
2020-12-14 19:40:08 -0600

    Pagina completa Nodejs con implementacion de expressjs,routes,ejs,bootstrap,Font Awesome, Google Fonts y Animate CSS.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ const path = require('path'); //manejador de working directory sin importar OS.
 //settings
 app.set('port',4000);
 app.set('views',path.join(__dirname,'views'));
-//app.engine('html',require('ejs').renderFile);
+app.engine('html',require('ejs').renderFile);
 app.set('view engine','ejs'); //No se necesita instanciarlo, motor de plantillas EJS ya esta integrado en Express js.
 
 //middlewares
 
 //routes
 app.use(require('./routes/index'));
-//static files
 
+//static files
+app.use(express.static(path.join(__dirname,'public')))
 
 //listening the server
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {    
-    res.render('index', {title: 'First Node.js website'});
+    res.render('index.html', {title: 'First Node.js website'});
 });
 
 router.get('/contact', (req,res) => {    
-    res.render('contact', {title: 'Contact page'});
+    res.render('contact.html', {title: 'Contact page'});
 });
 
 module.exports = router;
